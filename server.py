b1f5b4739ead49f3da5c03aec7f913d3d0b08804
Jonathan McKenzie <jonathan.t.mckenzie@gmail.com>
2012-06-12 19:37:13 -0400

    Fixing bugs with the way js was being referenced.

--- server.py
+++ server.py
@@ -1,5 +1,10 @@
-from bottle import route, run, jinja2_template as template
+from bottle import route, run, jinja2_template as template, static_file, url
 import lrc
+
+
+@route("/static/:path#.+#", name="static")
+def static(path):
+    return static_file(path, root="static")
 
 
 @route("/play/:name")
@@ -10,6 +15,7 @@ def load_game(name=None):
     line_count = len(lrc_object.lines)
     line_enumeration = enumerate(lrc_object.lines)
     return template("game.html", name=name, lrc_object=lrc_object,
-            line_count=line_count, line_enumeration=line_enumeration)
+            line_count=line_count, line_enumeration=line_enumeration,
+            get_url=url)
 
 run(host='localhost', port=8080)
